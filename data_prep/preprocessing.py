cee486d0f2b0db0068c92745359afd2f65f43c3c
Manish Kumar <sihagmanish36@gmail.com>
2023-03-05 00:50:16 -0600

    Updated preprocessing scripts

--- data_prep/preprocessing.py
+++ data_prep/preprocessing.py
@@ -16,12 +16,10 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 import contractions
 from spellchecker import SpellChecker
 
-# nltk.download('words')
-# nltk.download('averaged_perceptron_tagger')
-# nltk.download('wordnet')
-# nltk.download('omw-1.4')
-# nltk.download('punkt') 
-# nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('punkt') 
+nltk.download('stopwords')
 
 # initialize WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
